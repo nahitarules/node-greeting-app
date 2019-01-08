6e53cc01b5bdae857db8af8c0dd752c0e323b79b
nahome <nahom.teshome@yahoo.com>
2019-01-07 22:22:05 -0800

    basic node app built

---
+++ goodbyes.js
@@ -0,0 +1,15 @@
+exports.goodbyesEnglish = function(){
+ console.log("Bye");
+}
+
+exports.goodbyesJapanese = function(){
+ console.log("sayonara");
+}
+
+exports.goodbyesSpanish = function(){
+ console.log("adios");
+}
+
+exports.goodbyesIcelandic = function(){
+ console.log("ciao");
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+const greetings = require("./greeting.js");
+const goodbye = require("./goodbyes.js")
+
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbye.goodbyesEnglish();
+
+goodbye.goodbyesJapanese();
+
+goodbye.goodbyesSpanish();
+
+goodbye.goodbyesIcelandic();
